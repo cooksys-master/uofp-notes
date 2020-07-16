5f38343f20e38430867f62018e066dca4fcc9154
Will Marttala <wmarttala123@gmail.com>
2020-07-16 12:42:14 -0500

    Added the rest of the alphabet and finished the implementation for countLetterOccurrences

--- collections-continued/src/main/java/com/cooksys/collections/continued/Application.java
+++ collections-continued/src/main/java/com/cooksys/collections/continued/Application.java
@@ -10,34 +10,35 @@ import java.util.Set;
 import com.cooksys.collections.continued.hotel.*;
 
 public class Application {
-	
-	private static final Set<Character> ALPHABET = new HashSet<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'));
-	
+
+	private static final Set<Character> ALPHABET = new HashSet<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+			'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'));
+
 	public static void main(String[] args) {
-		
+
 		System.out.println(countUniqueLetters("Hello World12345"));
 		System.out.println(countUniqueLetters("supercalifragilisticexpialidocious"));
-		
+
 		System.out.println(countLetterOccurrences("supercalifragilisticexpialidocious"));
-		
+		System.out.println(countLetterOccurrences("Hello World12345;!@"));
+
 		Adult adult1 = new Adult("Will", 30);
 		Adult adult2 = new Adult("Peter", 30);
-		
+
 		Set<Person> guests = new HashSet<>();
 		guests.add(adult1);
 		guests.add(adult2);
-		
+
 		Hotel hotel1 = new Hotel(20);
-		
+
 		hotel1.checkInGuests(100, guests);
-		
+
 		System.out.println(hotel1.roomsStillAvailable());
-		
+
 		System.out.println(hotel1.allCheckedInGuests());
-		
-		
+
 	}
-	
+
 	public static int countUniqueLetters(String s) {
 		Set<Character> uniqueLetters = new HashSet<>();
 		for (char c : s.toLowerCase().toCharArray()) {
@@ -47,14 +48,16 @@ public class Application {
 		}
 		return uniqueLetters.size();
 	}
-	
+
 	public static Map<Character, Integer> countLetterOccurrences(String s) {
 		Map<Character, Integer> result = new HashMap<>();
-		for (char c : s.toCharArray()) {
-			if (result.containsKey(c)) {
-				result.put(c, result.get(c) + 1);
-			} else {
-				result.put(c, 1);
+		for (char c : s.toLowerCase().toCharArray()) {
+			if (ALPHABET.contains(c)) {
+				if (result.containsKey(c)) {
+					result.put(c, result.get(c) + 1);
+				} else {
+					result.put(c, 1);
+				}
 			}
 		}
 		return result;
