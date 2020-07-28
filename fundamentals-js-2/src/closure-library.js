10b9972c96aa5fbd8db3c8bb3970a4532261b2ac
Peter Zastoupil <peterzastoupil@gmail.com>
2020-07-28 12:54:23 -0500

    added js fundamentals day 2 notes

---
+++ fundamentals-js-2/src/closure-library.js
@@ -0,0 +1,15 @@
+let counter = 0
+
+export function inc () {
+  counter++
+}
+
+export function dec () {
+  counter--
+}
+
+export function getCounter () {
+  return counter
+}
+
+export default counter
