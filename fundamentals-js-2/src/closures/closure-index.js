db84fbe3702046c398e34c3c95d8237fca5c9594
Peter Zastoupil <peterzastoupil@gmail.com>
2020-07-29 10:05:02 -0500

    reorganized examples into folders

---
+++ fundamentals-js-2/src/closures/closure-index.js
@@ -0,0 +1,10 @@
+import counter, { inc, dec, getCounter } from './closure-library';
+
+// console.log(getCounter())
+console.log(counter);
+inc();
+// console.log(getCounter())
+console.log(counter);
+dec();
+// console.log(getCounter())
+console.log(counter);
