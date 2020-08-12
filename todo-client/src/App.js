44b3d7b89e98753590c530be70402c16aae22943
Will Marttala <wmarttala123@gmail.com>
2020-08-12 17:06:06 -0500

    Added functionality for creating, completing, and deleting todos

--- todo-client/src/App.js
+++ todo-client/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 function App() {
   const [todos, setTodos] = React.useState([]);
+  const [inputText, setInputText] = React.useState('');
 
   React.useEffect(() => {
     fetch('http://localhost:8080/todos/')
@@ -10,15 +11,70 @@ function App() {
       .then(data => setTodos(data));
   }, []);
 
+  const handleCreateTodo = keyPressEvent => {
+    if (keyPressEvent.key === 'Enter') {
+      fetch(`http://localhost:8080/todos/?message=${inputText}`, {
+        method: 'POST'
+      })
+        .then(response => response.json())
+        .then(newTodo => setTodos([...todos, newTodo]));
+      setInputText('');
+    }
+  };
+
+  const handleToggleTodo = todo => {
+    const todoUpdate = { message: todo.message, completed: !todo.completed };
+    console.log(todoUpdate);
+    fetch(`http://localhost:8080/todos/${todo.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(todoUpdate)
+    })
+      .then(response => response.json())
+      .then(updatedTodo =>
+        setTodos([
+          ...todos.filter(todo => todo.id !== updatedTodo.id),
+          updatedTodo
+        ])
+      );
+  };
+
+  const handeDelete = id => {
+    fetch(`http://localhost:8080/todos/${id}`, {
+      method: 'DELETE'
+    })
+      .then(response => response.json())
+      .then(deletedTodo =>
+        setTodos(todos.filter(todo => todo.id !== deletedTodo.id))
+      );
+  };
+
   return (
     <div className="TodoApp">
       <h1 className="Header">todos</h1>
-      <input type="text" onClick={() => alert('hi')} />
+      <input
+        type="text"
+        value={inputText}
+        onChange={event => {
+          setInputText(event.target.value);
+        }}
+        onKeyPress={handleCreateTodo}
+      />
       <div className="TodoList">
         {todos.map(todo =>
-          <p key={todo.id}>
-            {todo.message}
-          </p>
+          <div className="Todo" key={todo.id}>
+            <input
+              type="radio"
+              checked={todo.completed}
+              onChange={() => handleToggleTodo(todo)}
+            />
+            <p>
+              {todo.message}
+            </p>
+            <button onClick={() => handeDelete(todo.id)}>X</button>
+          </div>
         )}
       </div>
     </div>
